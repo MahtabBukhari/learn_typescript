f86d44c77635bf5ada73301b920382ac3b99c94b
MahtabBukhari <git config --global user.email you@example.com>
2022-09-03 08:21:37 +0500

    react component childern of other component

---
+++ src/components/PageTwoComp3.tsx
@@ -0,0 +1,13 @@
+
+
+type childProps={
+    children:React.ReactNode
+}
+
+const PageTwoComp3 = (props:childProps) => {
+  return (
+    <div>{props.children}</div>
+  )
+}
+
+export default PageTwoComp3
--- src/pages/PageTwo.tsx
+++ src/pages/PageTwo.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PageTwoComp from '../components/PageTwoComp'
 import PageTwoComp2 from '../components/PageTwoComp2'
+import PageTwoComp3 from '../components/PageTwoComp3'
 
 function PageTwo() {
   return (
@@ -8,6 +9,10 @@ function PageTwo() {
       <PageTwoComp status="loading"/>
       {/* passing data as children to copmonent */}
       <PageTwoComp2>this text is working as child of this component</PageTwoComp2>
+
+      {/* passing react component as child to other component */}
+
+      <PageTwoComp3> <PageTwoComp2>component as child of other component</PageTwoComp2> </PageTwoComp3>
     </div>
   )
 }
